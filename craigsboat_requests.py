38771e435b21c1327e5793b16ef342952cb44af4
Conway Yao <conway.yao@fiscalnote.com>
2016-11-21 23:45:38 -0500

    Completed the Craigsboat_requests script
    Completed the craigsboat_requests script, which writes the
    craigsboat_results.csv file

--- craigsboat_requests.py
+++ craigsboat_requests.py
@@ -5,26 +5,38 @@ Created on Sun Nov 20 16:08:16 2016
 @author: conway.yao
 """
 
-import requests
-from bs4 import BeautifulSoup
-import re
-
-results_base= 'https://washingtondc.craigslist.org/search/boo'
-
-r = requests.get(results_base)
-soup = BeautifulSoup(r.text, "lxml")
-
-# Extract the section that contains the information for each listing
-links = soup.find_all('li', class_='result-row')
+import requests 
+from bs4 import BeautifulSoup 
+import re  
+import time
+import pandas as pd
 
 # Initialize a list that will contain the information about each listing that we collect
 results = []
 
-# For loop that extracts information from each listing on the listings page
-for listing in links[0:5]:
+# Manually list out each listings page's endpoint
+results_endpoints = ['', '?s=100', '?s=200', '?s=300', '?s=400', '?s=500']
+
+
+## Defines a function which scrapes all listings pages
+def scrape_listings(page):
+    results_base= 'https://washingtondc.craigslist.org/search/boo'
+    r = requests.get(results_base + page)
+    soup = BeautifulSoup(r.text, "lxml")
+    
+    # Extract the section that contains the information for each listing
+    links = soup.find_all('p', class_='result-info')
+    
+    # Call get_listings_info to extract each listing's info
+    for listing in links:
+        get_listings_info(listing)
+
+
+## Defines a function which extracts information from each listing on the listings page
+def get_listings_info(listing):
 
     # Extract listing_url to pass to get_boat_info function
-    listing_link = listing.find(class_ = 'result-title hdrlnk').attrs['href']
+    listing_link = listing.find(class_='result-title hdrlnk').attrs['href']
     listing_base = 'https://washingtondc.craigslist.org'
     listing_url = listing_base + listing_link
 
@@ -32,13 +44,24 @@ for listing in links[0:5]:
     boat_info = get_boat_info(listing_url)
 
     # Populate dictionary with further information
-    boat_info['price'] = listing.find(class_ = 'result-price').text
-    boat_info['title'] = listing.find(class_ = 'result-title hdrlnk').text
-    boat_info['id'] = listing.find(class_ = 'result-title hdrlnk').attrs['data-id']
+    boat_info['title'] = listing.find(class_='result-title hdrlnk').text
+    boat_info['id'] = listing.find(class_='result-title hdrlnk').attrs['data-id']
     boat_info['url'] = listing_url
+    boat_info['date'] = listing.find(class_='result-date').attrs['datetime']
+    try:
+        boat_info['price'] = listing.find(class_ ='result-price').text
+        boat_info['region'] = listing.find(class_='result-hood').text
+    except:
+        pass
+    
+    # Return dictionary into results list
+    results.append(boat_info)
+    
+    # Sleep 'for' loop for 0.1 seconds (10 requests/sec)
+    time.sleep(0.1)
 
-## Function to extract info from a single listing page
-
+    
+## Defines a function to extract info from a single listing page
 def get_boat_info(listing_url):
 
     # Use BS4 to parse page
@@ -65,3 +88,18 @@ def get_boat_info(listing_url):
 
     # Return the dictionary that contains the listing's information
     return(boat_attrs)
+    
+
+## Run the program!
+for endpoint in results_endpoints:
+    scrape_listings(endpoint)
+    print(len(results))
+    print (' collected after scraping ' + endpoint + ' page.')
+    print('\n')
+
+
+## Convert results to pandas DataFrame and export as CSV
+resultsDF = pd.DataFrame(results)
+resultsDF.to_csv('craigsboat_results.csv', encoding='utf-8')
+
+    
